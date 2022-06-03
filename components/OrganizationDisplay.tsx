53c8e8fb1cd0314a24749273c1fc5f5536ec18a0
Daniel <danieldawit33@gmail.com>
2022-06-02 23:28:43 -0400

    add image list

--- components/OrganizationDisplay.tsx
+++ components/OrganizationDisplay.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Image, View} from "react-native";
+import {Image, View, Text} from "react-native";
 import {Organization} from "../database/modules/organizations/Organization";
+import Layout from "../constants/Layout";
 
 interface OrganizationDisplayProps {
     organization: Organization;
@@ -22,13 +23,40 @@ function OrganizationDisplay({organization}: OrganizationDisplayProps) {
     return (
         <View>
 
-            <Image
-                source={{
-                    uri: image
-                }}
-                width={'20%'}
-                height={'20%'}
-            />
+            {
+                image ?
+                <Image
+                    style={{
+                        width: Layout.window.width,
+                        height: Layout.window.height * 0.25,
+                        marginVertical: 10
+                    }}
+                    source={{
+                        uri: image || 'data:'
+                    }}
+                    resizeMode={'cover'}
+                />   : null
+
+            }
+
+            <View style={{
+                position: 'absolute',
+                bottom: 5,
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                width: '100%',
+                flex: 1,
+                padding: 5
+            }}>
+                <Text style={{
+                    color: 'white',
+                    fontWeight: '600',
+                    fontSize: 15
+                }}>{name}</Text>
+                <Text numberOfLines={3} adjustsFontSizeToFit style={{
+                    color: 'white',
+                    fontWeight: '500'
+                }}>{description}</Text>
+            </View>
 
         </View>
     );
--- screens/AuctionSelect.tsx
+++ screens/AuctionSelect.tsx
@@ -1,23 +1,33 @@
-import {FlatList, ListRenderItem, StyleSheet} from 'react-native';
+import {FlatList, ListRenderItem, StyleSheet, Text} from 'react-native';
 import {View} from '../components/Themed';
 import {useState} from "react";
 import {db} from "../database";
 import OrganizationDisplay from "../components/OrganizationDisplay";
+import {Organization} from "../database/modules/organizations/Organization";
 
 export default function AuctionSelect() {
 
-    const [orgIDs, setIDs] = useState<string[]>(db.organizations?.winners ?? []);
+    const [selectedIDs, setSelected] = useState<string[]>(db.organizations?.winners ?? []);
 
-    const renderOrg: ListRenderItem<string> = ({item}) => {
-        const org = db.organizations?.orgs.get(item);
-        if(!org) return null;
+    const [organizations, setOrgs] = useState(db.organizations?.orgs ?? new Map());
+
+    const renderOrg: ListRenderItem<[string, Organization]> = ({item}) => {
+        const [, org] = item;
+        if (!org) return null;
+
         return <OrganizationDisplay organization={org}/>
     }
+
 
     return (
         <View style={styles.container}>
 
-            <FlatList data={orgIDs} renderItem={renderOrg} keyExtractor={id => id}/>
+            <View style={{flex: 1}}>
+                <FlatList data={[...organizations]}
+                          renderItem={renderOrg}
+                          keyExtractor={(org: [string, Organization]) => org[0]}/>
+            </View>
+
 
         </View>
     );
